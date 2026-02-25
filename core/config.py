436589342f5515505e3b23f621caa4058b83b550
shrikantApp <shrikant@appristine.in>
2026-02-25 12:38:09 +0530

    code push

--- core/config.py
+++ core/config.py
@@ -4,14 +4,14 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Coach Speech"
     # PostgreSQL Database URL
-    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://postgres:password@localhost:5432/coachspeech")
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://coach:FufeyH7ohb.r7aAedtc60G3@13.200.216.93:5432/coach")
     
     # JWT Secrets
     SECRET_KEY: str = os.getenv("SECRET_KEY", "super-secret-key-keep-safe")
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 3000
     
     # OpenAI API Key
-    OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY", "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")
+    OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY", "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")
 
 settings = Settings()
